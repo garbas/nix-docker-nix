bfaffb4d9629a67488984d991792c13df94e95d4
Rok Garbas <rok@garbas.si>
2018-10-31 12:23:45 +0100

    lets commit this before i forget

--- default.nix
+++ default.nix
@@ -1,34 +1,24 @@
-{ nix ? builtins.fetchGit ./../nix
-, channel ? "nixos-18.09"
-, nixpkgs ? builtins.fetchGit { url = https://github.com/NixOS/nixpkgs-channels.git; ref = channel; }
-, officialRelease ? false
-, systems ? [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ]
+{ pkgs ? import <nixpkgs> {}
+, nix ? pkgs.nix
 }:
 
 let
-  pkgs = import nixpkgs { system = builtins.currentSystem or "x86_64-linux"; };
-  jobs = import ./../nix/release.nix { inherit nix nixpkgs officialRelease systems; };
-
   inherit (pkgs) stdenv;
   inherit (pkgs.lib) concatStringsSep genList;
 
+  channel = "nixos-${builtins.readFile "${pkgs.path}/.version"}";
+
   channelUrl = "https://github.com/NixOS/nixpkgs-channels/archive/${channel}.tar.gz";
-  passwd = ''
-    root:x:0:0::/root:/run/current-system/sw/bin/bash
-    ${concatStringsSep "\n" (genList (i: "nixbld${toString (i+1)}:x:${toString (i+30001)}:30000::/var/empty:/run/current-system/sw/bin/nologin") 32)}
+
+  nixVersion = (builtins.parseDrvName pkgs.nix.name).version;
+
+  nixpkgsChannel = pkgs.runCommand "nixpkgs-channel" {} ''
+    mkdir -p $out/nixpkgs
+    cp -R ${pkgs.path}/* $out/nixpkgs/
+    cp -R ${pkgs.path}/.version $out/nixpkgs/
   '';
 
-  group = ''
-    root:x:0:
-    nogroup:x:65534:
-    nixbld:x:30000:${concatStringsSep "," (genList (i: "nixbld${toString (i+1)}") 32)}
-  '';
-
-  nsswitch = ''
-    hosts: files dns myhostname mymachines
-  '';
-
-  nix-verify = stdenv.mkDerivation {
+  nixVerify = stdenv.mkDerivation {
     name = "nix-verify";
     src = ./nix-verify.sh;
     buildCommand = ''
@@ -42,37 +32,42 @@ let
     '';
   };
 
-  nixpkgsChannel = pkgs.runCommand "nixpkgs-channel" {} ''
-    mkdir -p $out/nixpkgs
-    cp -R ${nixpkgs}/* $out/nixpkgs/
-    cp -R ${nixpkgs}/.version $out/nixpkgs/
-  '';
-
-
-  defaultConfig ={
+  # TODO: config could be using nixos modules
+  defaultConfig = {
     Env = [
       "HOME=/root"
       # TODO: PATH should be calculated from contents
       "PATH=/root/.nix-profile/bin"
       "NIX_PAGER=cat"
       "NIX_PATH=nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixpkgs"
-      "GIT_SSL_CAINFO=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-      "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+      "GIT_SSL_CAINFO=/root/.nix-profile/etc/ssl/certs/ca-bundle.crt"
+      "SSL_CERT_FILE=/root/.nix-profile/etc/ssl/certs/ca-bundle.crt"
     ];
+    Cmd = [ "bash" ];
+    WorkingDir = "/root";
   };
 
-  mkContentsEnv = nix: contents: pkgs.buildEnv {
+  # TODO: maybe we should use busybox instead of coreutils
+  mkNixEnv = nix: contents: pkgs.buildEnv {
     name = "user-environment";
     paths = [
       nix
       # nix-store uses cat program to display results as specified by
       # the image env variable NIX_PAGER.
       pkgs.coreutils
+      # nix-channel/nix-build/nix-env needs SSL_CERT_FILE set to be able to
+      # download from binary cache. we also set GIT_SSL_CAINFO.
       pkgs.cacert
+      # TODO: write why we need it
       nixpkgsChannel
-      pkgs.bashInteractive # TODO: maybe busybox
+      # TODO: write why we need it
+      pkgs.bashInteractive
     ] ++ contents;
+    # TODO: create manifest.nix
+    #postBuild = ''
+    #'';
   };
+
   buildImageWithNix = args@{ contents ? []
                            , runAsRoot ? ""
                            , nix ? pkgs.nix
@@ -80,7 +75,7 @@ let
                            , ...
                            }:
     let
-      contentsEnv = mkContentsEnv nix contents;
+      contentsEnv = mkNixEnv nix contents;
     in
     (pkgs.dockerTools.buildImageWithNixDb (args // {
       inherit config;
@@ -88,57 +83,73 @@ let
         #!${stdenv.shell}
         ${pkgs.dockerTools.shadowSetup}
 
+        # Create root user
         mkdir -p /etc
-        # TODO: why do we need this files explain in comments
-        echo '${passwd}' > /etc/passwd
-        echo '${group}' > /etc/group
-        echo '${nsswitch}' > /etc/nsswitch.conf
+        echo 'root:x:0:0::/root:/root/.nix-profile/bin/bash' > /etc/passwd
+        echo 'root:x:0:' > /etc/group
 
+        # TODO: why do we need this files explain in comments
+        mkdir -p /etc
+        echo 'hosts: files dns myhostname mymachines' > /etc/nsswitch.conf
+
+        # Create temporary folder
         mkdir /tmp
         chmod 1777 /tmp
 
-        export HOME=/root
-        mkdir -p /root/.nix-defexpr
-        ${contentsEnv}/bin/nix-channel --add https://nixos.org/channels/${channel} nixpkgs
+        # Subscribe to Nix channel
+        mkdir -p /root
+        echo 'https://nixos.org/channels/${channel} nixpkgs' > /root/.nix-channels
 
-        # this is needed to make nix-channel work
+        # Create initial channel
+        mkdir -p /nix/var/nix/profiles/per-user/root /root/.nix-defexpr
         ln -s ${nixpkgsChannel} /nix/var/nix/profiles/per-user/root/channels-1-link
         ln -s channels-1-link /nix/var/nix/profiles/per-user/root/channels
         ln -s /nix/var/nix/profiles/per-user/root/channels /root/.nix-defexpr/channels_root
 
-        # things installed with nix-env go to /nix/var/nix/profiles/default
-        # we need to create ~/.nix-profile symlink manually
-        ln -s /nix/var/nix/profiles/default /root/.nix-profile
-
-        # TODO: add description
+        # Create default profile for
         mkdir -p /nix/var/nix/profiles
-        ln -s ${contentsEnv} /nix/var/nix/profiles/default-1-link
-        ln -s default-1-link /nix/var/nix/profiles/default
+        ln -s ${contentsEnv} /nix/var/nix/profiles/per-user/root/default-1-link
+        ln -s default-1-link /nix/var/nix/profiles/per-user/root/default
+        # things installed with nix-env go to /nix/var/nix/profiles/per-user/root/default
+        # we need to create ~/.nix-profile symlink manually
+        ln -s /nix/var/nix/profiles/per-user/root/default /root/.nix-profile
+
 
         mkdir -p /nix/var/nix/gcroots
         ln -s /nix/var/nix/profiles /nix/var/nix/gcroots/profiles
 
         # Make the shell source nix.sh during login.
-        nix_profile=$HOME/.nix-profile/etc/profile.d/nix.sh
-        echo "if [ -e $nix_profile ]; then . $nix_profile; fi" >> "$HOME/.bash_profile"
+        nix_profile=/root/.nix-profile/etc/profile.d/nix.sh
+        echo "if [ -e $nix_profile ]; then . $nix_profile; fi" >> /root/.bash_profile
       '' + runAsRoot;
     }));
 
-  nix_version = (builtins.parseDrvName pkgs.nix.name).version;
-
 in
 {
+  inherit nixVerify
+          nixpkgsChannel
+          mkNixEnv
+          buildImageWithNix;
   docker = {
     noSandbox = buildImageWithNix {
       name = "nix";
-      tag = "${nix_version}-no-sandbox";
+      tag = nixVersion;
       contents = [
         # used for testing, for now
-        nix-verify
+        nixVerify
         # temporary remove when done
         pkgs.tree
         pkgs.gnugrep
       ];
     };
+    # TODO: withSandbox = buildImageWithNixSandbox {
+    #   name = "nix-sandbox";
+    #   tag = nixVersion;
+    # };
+    # TODO: withDeamon = buildImageWithNixDaemon {
+    #   name = "nix-daemon";
+    #   tag = nixVersion;
+    # };
+
   };
 }
